26d9405f28c174b1acbfc70f88f9ea74fb98e523
hzwer <598460606@163.com>
2015-11-24 20:28:06 +0800

    Deal with some exceptional cases

--- spider-hzwer.py
+++ spider-hzwer.py
@@ -5,7 +5,6 @@ __author__ = 'hzwer'
 
 import re
 from urllib.request import *
-from urllib.error import *
 
 home = 'http://hzwer.com/'
 tot = 0
@@ -17,17 +16,17 @@ for i in range(1, 8000):
         request = Request(url)
         response = urlopen(request)
         content = response.read().decode("UTF-8").replace("\r\n", "\n")
-
-    except HTTPError as e:
-        continue
-        
+    except:
+        pass
     else:
         pattern = re.compile('-webkit-tab-size:4; tab-size:4; font-size: 15px !important; line-height: 16px !important;">\n(.*?)</textarea>', re.S)
         code = re.findall(pattern, content)
         if(code):
-            length = len(code[-1].splitlines())
-            print('url: {} {}行代码'.format(url, length))
+            length = 0
+            for j in code:
+                length = length + len(j.splitlines())
             tot += length
+            print('url: {} {}行代码，合计{}行代码'.format(url, length, tot))
             page = page + 1
 
 print('共{}个页面，{}行代码'.format(page, tot))
